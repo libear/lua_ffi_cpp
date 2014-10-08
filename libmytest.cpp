38a0da8f6ebd8161852854ac616fdab47edb2262
libearxiong <libear@users.noreply.github.com>
2014-10-08 22:18:11 +0800

    Update libmytest.cpp

--- libmytest.cpp
+++ libmytest.cpp
@@ -3,14 +3,14 @@
 class TEST_CLASS {
     public: 
 	int ret_int(){
-	    return 100;
+	    return 200;
 	}
 };
 
 extern "C" int add(int a, int b)
 {
     TEST_CLASS test_class;
-    int c = 100;
+    int c;
     c = test_class.ret_int();
     return a + b + c;
 }
